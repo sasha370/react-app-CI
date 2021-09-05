d393184b479cad42c4afc4a13fcac67bd55bcb32
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2021-09-05 18:45:39 +0300

    fix(normal): normal

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn test
+          Learn React test
         </a>
       </header>
     </div>
