42f0051b6b7be39e080e7c7e2745c1476fd8152d
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2021-09-06 11:19:19 +0300

    fix(fix): added async

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3746,24 +3746,28 @@ exports.getState = getState;
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-try{
-  const token = core.getInput('token');
-  const title = core.getInput('title');
-  const body = core.getInput('body');
-  const assignees = core.getInput('assignees');
+async  function  run() {
+  try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
 
-  const octokit = new github.getOctokit(token);
-  const response = octokit.rest.issues.create({
-    ...github.context.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split('\n') : undefined
-  });
+    const octokit = new github.getOctokit(token);
+    const response = await octokit.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined
+    });
 
-  setOutput('issue', JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error.message);
+    core.setOutput('issue', JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
 
 
 /***/ }),
--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,21 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try{
-  const token = core.getInput('token');
-  const title = core.getInput('title');
-  const body = core.getInput('body');
-  const assignees = core.getInput('assignees');
+async  function  run() {
+  try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
 
-  const octokit = new github.getOctokit(token);
-  const response = octokit.rest.issues.create({
-    ...github.context.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split('\n') : undefined
-  });
+    const octokit = new github.getOctokit(token);
+    const response = await octokit.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined
+    });
 
-  setOutput('issue', JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error.message);
+    core.setOutput('issue', JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
