ee5197809c7d805c6d83679cee7d26b7246a58b4
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2021-09-07 13:26:31 +0300

    test(t): t

--- .github/actions/slack/script.rb
+++ .github/actions/slack/script.rb
@@ -5,7 +5,7 @@ def send_slack_message
   path = ARGV[0]
   msg = ARGV[1] #TODO понять как передовать текст
   responce = %x{curl -X POST -H 'Content-type: application/json' --data '{"text":"#{msg}"}' "#{path}"}
-
+  puts "======= #{responce}"
   return puts 'error' until responce == 'ok'
 end
 
